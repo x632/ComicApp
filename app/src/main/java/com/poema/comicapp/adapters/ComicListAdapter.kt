9967e291bd61b81f715670295463ec1866069e93
Andreas Svedstedt <andreas@poema.se>
2021-09-12 08:49:27 +0200

    removed databinding -realized that it could not work with the filering functions. Kept the other changes, though

--- app/src/main/java/com/poema/comicapp/adapters/ComicListAdapter.kt
+++ app/src/main/java/com/poema/comicapp/adapters/ComicListAdapter.kt
@@ -9,13 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.poema.comicapp.R
 import com.poema.comicapp.model.ComicListItem
 import com.poema.comicapp.ui.activities.DetailActivity
 import com.poema.comicapp.R.*
-import com.poema.comicapp.databinding.ComicItemLayoutBinding
 
 
 class ComicListAdapter(private val context: Context) :
@@ -25,19 +22,26 @@ class ComicListAdapter(private val context: Context) :
     private lateinit var comicList: List<ComicListItem>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComicItemViewHolder {
-        val layoutInflater : LayoutInflater = LayoutInflater.from(parent.context)
-        val comicItemLayoutBinding : ComicItemLayoutBinding = ComicItemLayoutBinding.inflate(layoutInflater, parent, false)
-        return ComicItemViewHolder(comicItemLayoutBinding)
+        return ComicItemViewHolder(
+            LayoutInflater.from(context).inflate(layout.comic_item_layout, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ComicItemViewHolder, position: Int) {
         val comicListItem = comicList[position]
-        holder.bind(comicListItem)
-
+        holder.tv1.text = comicListItem.title
+        holder.tv2.text = comicListItem.date
+        holder.tv3.text = comicListItem.id.toString()
         holder.itemView.setOnClickListener {
+
             val intent = Intent(context, DetailActivity::class.java)
             intent.putExtra("id", comicListItem.id)
             context.startActivity(intent)
+        }
+        if (!comicListItem.isFavourite) {
+            holder.ivheart.setImageResource(drawable.ic_baseline_favorite_border_24)
+        } else {
+            holder.ivheart.setImageResource(drawable.ic_baseline_favorite_24)
         }
     }
 
@@ -47,24 +51,14 @@ class ComicListAdapter(private val context: Context) :
 
     fun submitList(list: MutableList<ComicListItem>) {
         comicList = list
-
+        notifyDataSetChanged()
     }
 
 
-    inner class ComicItemViewHolder(val binding: ComicItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(comicListItem: ComicListItem){
-            binding.comicListItem = comicListItem
-        }
-
-
-    }
-}
-
-@BindingAdapter("android:setDrawable")
-fun setDrawable(imageView: ImageView, isFavourite: Boolean) {
-    if (isFavourite) {
-        imageView.setImageResource(R.drawable.ic_baseline_favorite_24)
-    } else {
-        imageView.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+    inner class ComicItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tv1: TextView = itemView.findViewById(id.tvTitle)
+        val tv2: TextView = itemView.findViewById(id.tvDate)
+        val tv3: TextView = itemView.findViewById(id.tvNumber)
+        val ivheart: ImageView = itemView.findViewById(id.iv_heart)
     }
 }
